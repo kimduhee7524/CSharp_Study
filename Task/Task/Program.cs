2f63d527f41ec9c936ab7e098f808dc9b412d829
Doit <77947280+kimduhee7524@users.noreply.github.com>
2024-02-23 17:55:28 +0900

    Task
    Task

---
+++ Task/Task/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UsingTask
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            TaskResult taskResult = new TaskResult();
+            taskResult.primenumber(2,100,4);
+        }
+
+        private void BasicTask()
+        {
+            // 1. Task.Start
+            Action someAction = () =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Printed asynchronously");
+            };
+            Task myTask = new Task(someAction);
+            myTask.Start();
+
+            Console.WriteLine("Printed synchronously");
+
+            myTask.Wait();
+
+            // 2. Task.Run 메소드는 생성과 시작을 단번에 함
+            Task myTask2 = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Printed async");
+            });
+            Console.WriteLine("Printed sync");
+            myTask2.Wait();
+
+            // 3. Task<TResult>
+            List<int> myList = new List<int> { 0, 1, 2 };
+
+            Task<List<int>> myTask3 = Task.Run(() =>
+            {
+                Thread.Sleep(1000); // 시뮬레이션을 위해 1초 대기
+                List<int> list = new List<int> { 3, 4, 5 };
+                return list;
+            });
+
+            myTask3.Wait();
+            myList.AddRange(myTask3.Result);
+        }
+
+    }
+}
---
+++ lock/lock/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace @lock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Thread incThread = new Thread(
+                new ThreadStart(counter.Increment));
+            Thread decThread = new Thread(
+                new ThreadStart(counter.Decrement));
+
+            incThread.Start();
+            decThread.Start();
+
+            incThread.Join();
+            decThread.Join();
+
+            Console.WriteLine(counter.Count);
+        }
+    }
+    class Counter
+    {
+        const int LOOP_COUNT = 1000;
+
+        readonly object thisLock;
+
+        private int count;
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public Counter()
+        {
+            thisLock = new object();
+            count = 0;
+        }
+
+        public void Increment()
+        {
+            int loopCount = LOOP_COUNT;
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    count++;
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        public void Decrement()
+        {
+            int loopCount = LOOP_COUNT;
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    count--;
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
